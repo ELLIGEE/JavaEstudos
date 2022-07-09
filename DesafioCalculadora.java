c23631a4b75a4cb7aff5fe58d99910adc5b98563
João Guilherme David <56896476+ELLIGEE@users.noreply.github.com>
2022-07-08 22:25:41 -0300

    Add files via upload

---
+++ DesafioCalculadora.java
@@ -0,0 +1,33 @@
+package operadores;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Infomre o número: ");
+		double num1 = entrada.nextDouble();
+		
+		System.out.println("Infomre o número: ");
+		double num2 = entrada.nextDouble();
+		
+		System.out.println("Infomre o número: ");
+		double op = entrada.nextDouble();
+		
+		// Lógica
+		double resultado = "+".equals(op) ? num1 + num2 : 0;
+		resultado = "-".equals(op) ? num1 - num2 : 0;
+		resultado = "*".equals(op) ? num1 * num2 : 0;
+		resultado = "/".equals(op) ? num1 / num2 : 0;
+		resultado = "%".equals(op) ? num1 % num2 : 0;
+		
+		System.out.printf("%.2f %s %.2f = %.2f", num1, op, num2, resultado);
+		
+		entrada.close();
+		
+		
+	
+	}
+}
---
+++ Desafioif.java
@@ -0,0 +1,18 @@
+package contorle;
+
+public class Desafioif {
+
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		if(nota >= 9.0) {
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!");
+		}
+		
+		/*if(nota <= 4.0) {
+			System.out.println("Ta reprovado fiot");
+		} */
+	}
+}
